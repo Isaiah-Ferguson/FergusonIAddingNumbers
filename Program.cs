d4497cbdab6dbedf25f2e9b11a11b88ddb38e6aa
Isaiah-Ferguson <crestice@yahoo.com>
2022-10-18 18:45:22 -0700

    finished read for Review

--- Program.cs
+++ Program.cs
@@ -1,36 +1,40 @@
 ﻿//Isaiah Keith Ferguson
-//10/17/2022
+//10/18/2022
 //adding numbers
+//We will be prompting the user for 2 numbers, if entered correctly
+//we will add them together and display the resolt.
+//Peer reviewed by:
 
 Console.Clear();
-int num1 = 0;
-int num2 = 0;
+double num1 = 0;
+double num2 = 0;
 string terminate = "";
-int total = 0;
+double total = 0;
 bool truenum1 = true;
 bool truenum2 = true;
 
 while(terminate != "end"){
-    Console.WriteLine("Please enter numbers");
+    Console.WriteLine("We will be addition on 2 numbers!");
+    Console.WriteLine("");
     Console.WriteLine("Enter your 1st number.");
 
 string numberOne = Console.ReadLine();
-truenum1 = Int32.TryParse(numberOne, out num1);
+truenum1 = double.TryParse(numberOne, out num1);
 
 Console.WriteLine("Enter your 2nd number.");
 string numberTwo = Console.ReadLine();
-truenum2 = Int32.TryParse(numberTwo, out num2);
+truenum2 = double.TryParse(numberTwo, out num2);
 
 if(truenum1 == true && truenum2 == true){
 
 total = num1 + num2;
 
- Console.WriteLine("the sum of these whole numbers is " + (num1 + num2));
+ Console.WriteLine("Your Total is  " + (num1 + num2));
 }else
 {
-    Console.WriteLine("Those aren't numbers.");
+    Console.WriteLine("Those aren't numbers you knuckhead!");
 }
 
- Console.WriteLine("To terminate the program type 'End'");
+Console.WriteLine("To terminate the program type 'End'or press 'enter' to replay.");
 terminate = Console.ReadLine().ToLower();
 } 
